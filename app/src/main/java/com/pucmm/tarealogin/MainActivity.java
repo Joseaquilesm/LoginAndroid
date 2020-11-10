a3dead1194e8ad80d785f340106ac8f0ada4b0be
Joseaquilesm <33287719+Joseaquilesm@users.noreply.github.com>
2020-11-10 00:31:24 -0400

    initial commit

---
+++ app/src/main/java/com/pucmm/tarealogin/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pucmm.tarealogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+    }
+}
